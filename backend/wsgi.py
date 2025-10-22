57ecd2afa8c5a512bb46b23c49cfb505d6ac9f08
Rahul93102 <rahuljha93102@gmail.com>
2025-10-23 00:39:25 +0530

    Update wsgi.py to use production settings when DJANGO_ENV=production

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,5 +1,11 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
+# Use production settings if DJANGO_ENV is set to 'production'
+if os.environ.get('DJANGO_ENV') == 'production':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings_prod')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
 application = get_wsgi_application()
