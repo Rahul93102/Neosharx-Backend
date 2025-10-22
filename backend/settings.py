fcefbd94b984ec53b0c9c4fed52707c6fa2f5d71
Rahul93102 <rahuljha93102@gmail.com>
2025-10-23 00:40:17 +0530

    Temporarily add Render domain to ALLOWED_HOSTS in dev settings to fix deployment

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-0qmja60_hcr1a-f1y54a7#k)dr8l*ma$)i4catatq!fn!hxqya
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend-neosharx.onrender.com', 'www.backend-neosharx.onrender.com']
 
 
 # Application definition
